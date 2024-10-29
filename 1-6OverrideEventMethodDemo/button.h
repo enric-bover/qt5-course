44de423824dfff9345d4dce64a987b708f6f7b39
Enric Bover <ebover.centelles@gmail.com>
2024-10-29 13:16:23 +0100

    Overriding event method demo

---
+++ 1-6OverrideEventMethodDemo/button.h
@@ -0,0 +1,20 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <QPushButton>
+
+class Button : public QPushButton
+{
+    Q_OBJECT
+public:
+    explicit Button(QWidget *parent = nullptr);
+
+signals:
+
+
+    // QObject interface
+public:
+    bool event(QEvent *event) override;
+};
+
+#endif // BUTTON_H
